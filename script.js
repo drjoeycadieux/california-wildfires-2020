fb0543a83cb020ccc1e514b99cbb0cdab4126dd9
Joey Cadieux <58401407+drjoeycadieux@users.noreply.github.com>
2021-12-06 12:40:17 -0500

    Updating...

---
+++ script.js
@@ -0,0 +1,39 @@
+const msg = [
+  "Whoops.",
+  "Oops.",
+  "Excuse me.",
+  "Oh Dear.",
+  "Well poo.",
+  "Hm...",
+  "This is awkward.",
+  "Well gosh!"
+];
+[
+  "It appears",
+  "Looks like",
+  "Unfortunately,",
+  "It just so happens",
+  "Sadly,",
+  "Seemingly from nowhere"
+],
+[
+  "there was an error.",
+  "we goofed up.",
+  "a bad thing happend.",
+  "the server crashed.",
+  "a bug appeared.",
+  "someone did a naughty.",
+  "pixies got into the server!",
+  "the server threw a tantrum.",
+  "the website had a bad day.",
+  "our code pooped out."
+],
+[
+  "Sorry.",
+  "Apologies.",
+  "Our bad.",
+  "Sad day.",
+  "We are quite contrite.",
+  "Beg pardon."
+]
+ ];
